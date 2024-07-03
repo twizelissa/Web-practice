f92297b2bbb03841346616100e345d830aa8c057
twizelissa <twizelissa@gmail.com>
2024-07-03 23:39:26 +0200

    added new changes

--- dom.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.js-subscribe-button').onclick = function() {
-    const buttonElement = document.querySelector('.js-subscribe-button'); 
-    if (buttonElement.innerHTML === 'subscribe') {
-        buttonElement.innerHTML = 'subscribed';
-    } else {
-        buttonElement.innerHTML = 'subscribe';
-    }
-};
